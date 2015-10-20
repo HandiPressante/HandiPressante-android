c108fdeb876c6966b1b01585bd10c8a72ac3152f
YvonnigJan <yvonnig.jan@insa-rennes.fr>
2015-10-20 16:00:23 +0200

    Ajout de la base de la map

--- app/src/main/java/com/handipressante/handipressante/MapFragment.java
+++ app/src/main/java/com/handipressante/handipressante/MapFragment.java
@@ -1,23 +1,74 @@
 package com.handipressante.handipressante;
 
-import android.support.v4.app.Fragment;
-import android.view.LayoutInflater;
-import android.os.Bundle;
+        import org.osmdroid.tileprovider.tilesource.ITileSource;
+        import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+        import org.osmdroid.views.overlay.MinimapOverlay;
+        import org.osmdroid.views.overlay.ScaleBarOverlay;
+        import org.osmdroid.views.overlay.compass.CompassOverlay;
+        import org.osmdroid.views.overlay.compass.InternalCompassOrientationProvider;
+        import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider;
+        import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay;
+
+        import android.annotation.TargetApi;
+        import android.content.Context;
+        import android.content.SharedPreferences;
+        import android.os.Build;
+        import android.os.Bundle;
+        import android.support.v4.app.Fragment;
+        import android.support.v4.app.FragmentManager;
+        import android.util.AttributeSet;
+        import android.util.DisplayMetrics;
+        import android.view.LayoutInflater;
+        import android.view.Menu;
+        import android.view.MenuInflater;
+        import android.view.MenuItem;
+        import android.view.MenuItem.OnMenuItemClickListener;
+        import android.view.SubMenu;
+        import android.support.v4.app.Fragment;
+
+
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
-/**
- * Created by Nico on 19/10/2015.
- */
-public class MapFragment extends Fragment {
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_map, container, false);
+import org.osmdroid.ResourceProxy;
+import org.osmdroid.api.IMapController;
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.util.ResourceProxyImpl;
+import org.osmdroid.views.MapView;
 
-        ((TextView) rootView.findViewById(android.R.id.text1)).setText(
-                getString(R.string.demo_map));
-        return rootView;
+
+public class MapFragment extends Fragment {
+
+    private ResourceProxy mResourceProxy;
+    private MapView mMapView;
+    private final static int ZOOM = 14;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
     }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        mResourceProxy = new ResourceProxyImpl(inflater.getContext().getApplicationContext());
+        mMapView = new MapView(inflater.getContext(), 256, mResourceProxy);
+
+        //Activer les boutons + et -
+        mMapView.setBuiltInZoomControls(true);
+        mMapView.setMultiTouchControls(true);
+
+        IMapController mapController = mMapView.getController();
+        //Choisir le niveau de zoom
+        mapController.setZoom(ZOOM);
+        //choisir le point centré du départ
+        GeoPoint startPoint = new GeoPoint(48.1199094, -1.6345865, 2944);
+        mapController.setCenter(startPoint);
+
+
+        return mMapView;
+    }
+
 }
