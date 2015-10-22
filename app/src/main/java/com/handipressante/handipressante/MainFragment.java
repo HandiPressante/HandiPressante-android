a768e74a05fceac3d5c34ede4758603d3b49f7f1
Nico <nleborgn@insa-rennes.fr>
2015-10-22 19:07:06 +0200

    Tab style

--- app/src/main/java/com/handipressante/handipressante/MainFragment.java
+++ app/src/main/java/com/handipressante/handipressante/MainFragment.java
@@ -1,11 +1,14 @@
 package com.handipressante.handipressante;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTabHost;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainFragment extends Fragment {
     private FragmentTabHost mTabHost;
@@ -25,11 +28,18 @@ public class MainFragment extends Fragment {
         mTabHost = new FragmentTabHost(getActivity());
         mTabHost.setup(getActivity(), getChildFragmentManager(), R.id.realtabcontent);
 
-        mTabHost.addTab(mTabHost.newTabSpec("tab1").setIndicator("Liste"),
+        mTabHost.addTab(mTabHost.newTabSpec("tab1").setIndicator(getTabIndicator(mTabHost.getContext(), R.string.tab_list_title)),
                 ListFragment.class, null);
-        mTabHost.addTab(mTabHost.newTabSpec("tab2").setIndicator("Carte"),
+        mTabHost.addTab(mTabHost.newTabSpec("tab2").setIndicator(getTabIndicator(mTabHost.getContext(), R.string.tab_map_title)),
                 MapFragment.class, null);
 
         return mTabHost;
+    }
+
+    private View getTabIndicator(Context context, int title) {
+        View view = LayoutInflater.from(context).inflate(R.layout.tab_layout, null);
+        TextView tv = (TextView) view.findViewById(R.id.textView);
+        tv.setText(title);
+        return view;
     }
 }
