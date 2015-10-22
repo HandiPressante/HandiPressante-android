a67834b632db73c389264d9bbaf92368ae6c8b6c
YvonnigJan <yvonnig.jan@insa-rennes.fr>
2015-10-22 19:17:02 +0200

    Centrage sur la position de l'utilisateur

--- app/src/main/java/com/handipressante/handipressante/MapFragment.java
+++ app/src/main/java/com/handipressante/handipressante/MapFragment.java
@@ -1,63 +1,76 @@
 package com.handipressante.handipressante;
 
+import org.osmdroid.tileprovider.tilesource.ITileSource;
+        import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+        import org.osmdroid.views.overlay.MinimapOverlay;
+        import org.osmdroid.views.overlay.ScaleBarOverlay;
+        import org.osmdroid.views.overlay.compass.CompassOverlay;
+        import org.osmdroid.views.overlay.compass.InternalCompassOrientationProvider;
+        import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider;
+        import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay;
 
-import android.content.Context;
-
-import android.content.res.Resources;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.drawable.Drawable;
-import android.location.Location;
-import android.location.LocationManager;
-import android.os.Build;
-import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.graphics.Canvas;
-import android.support.v4.content.ContextCompat;
-import android.support.v4.content.res.ResourcesCompat;
-import android.view.LayoutInflater;
-
+        import android.Manifest;
+        import android.annotation.TargetApi;
+        import android.content.Context;
+        import android.content.SharedPreferences;
+        import android.content.pm.PackageManager;
+        import android.location.Address;
+        import android.location.Geocoder;
+        import android.location.Location;
+        import android.location.LocationListener;
+        import android.location.LocationManager;
+        import android.location.LocationProvider;
+        import android.os.Build;
+        import android.os.Bundle;
+        import android.os.Message;
+        import android.support.v4.app.Fragment;
+        import android.support.v4.app.FragmentManager;
+        import android.support.v4.content.ContextCompat;
+        import android.util.AttributeSet;
+        import android.util.DisplayMetrics;
+        import android.util.Log;
+        import android.view.LayoutInflater;
+        import android.view.Menu;
+        import android.view.MenuInflater;
+        import android.view.MenuItem;
+        import android.view.MenuItem.OnMenuItemClickListener;
+        import android.view.SubMenu;
+        import android.support.v4.app.Fragment;
 
 
 import android.view.View;
-import android.view.ViewDebug;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import org.osmdroid.ResourceProxy;
 import org.osmdroid.api.IMapController;
-import org.osmdroid.bonuspack.kml.StyleMap;
-import org.osmdroid.bonuspack.overlays.Marker;
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
 import org.osmdroid.util.GeoPoint;
 import org.osmdroid.util.ResourceProxyImpl;
-import org.osmdroid.views.MapController;
 import org.osmdroid.views.MapView;
 
-import org.osmdroid.views.overlay.ItemizedIconOverlay;
-import org.osmdroid.views.overlay.ItemizedIconOverlay.OnItemGestureListener;
-import org.osmdroid.views.overlay.MyLocationOverlay;
-import org.osmdroid.views.overlay.OverlayItem;
-import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider;
-import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay;
+        import java.io.IOException;
+        import java.util.List;
+        import java.util.Locale;
+        import java.util.logging.Handler;
 
-import java.util.ArrayList;
-import java.util.LinkedList;
-
-//création de la vue Fragment
 public class MapFragment extends Fragment {
 
-    private MapView mMapView;
-    private MapController mMapController;
     private ResourceProxy mResourceProxy;
+    private MapView mMapView;
     private final static int ZOOM = 14;
+    //LocationManager locationManager = (LocationManager) Context.getSystemService(Context.LOCATION_SERVICE);
+    //private MyLocation mloc = new MyLocation();
+    //private MyLocation.LocationResult locResult;
+    private Location loc;
+    //MyLocation.LocationResult pos = mloc.getLocationResult();
 
-    //initialiser startPoint
-    public Location mylocation;
 
-    //Coordonnées par defaut
-    public LocationManager locationManager;
-    Location location = null;
-    ArrayList<OverlayItem> anotherOverlayItemArray;
+    public void setLoc(Location _loc){
+        loc = _loc;
+        System.out.println("--------------------------------------------------------------- Testo (loc) : "+ loc);
+    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -69,48 +82,35 @@ public class MapFragment extends Fragment {
         mResourceProxy = new ResourceProxyImpl(inflater.getContext().getApplicationContext());
         mMapView = new MapView(inflater.getContext(), 256, mResourceProxy);
 
-        //mMapView.setMaxZoomLevel(30); //pour augmenter le zoom
         //Activer les boutons + et -
         mMapView.setBuiltInZoomControls(true);
-        //activer le controle multitouch
         mMapView.setMultiTouchControls(true);
-        //Changer la couleur de la carte
-        mMapView.setTileSource(TileSourceFactory.MAPQUESTOSM);
+
         IMapController mapController = mMapView.getController();
+
         //Choisir le niveau de zoom
-        mMapView.setMaxZoomLevel(20);
         mapController.setZoom(ZOOM);
 
-        GpsMyLocationProvider imlp = new GpsMyLocationProvider(this.getContext());
-
-        // public void setLocationUpdateMinDistance(final float meters) (distance de mise a jour
-        imlp.setLocationUpdateMinDistance(10);
-
         //choisir le point centre du depart
-        GeoPoint startPoint = new GeoPoint(48.1199094, -1.6345865, 2944);
+        //GeoPoint startPoint = new GeoPoint(48.120227199999995, -1.6345466);
+
+        MyLocation mloc = new MyLocation();
+        /*System.out.println("--------------------------------------------------------------- Testo (mloc == null ?) : " + (mloc == null));
+        System.out.println("--------------------------------------------------------------- Testo (mloc.locationResult == null ?): " + (mloc.locationResult == null));*/
+        mloc.locationResult.setMap(this);
+        mloc.searchLocation(getContext(), mloc.locationResult);
+        try {
+            Thread.sleep(600);
+            System.out.println("-------------------- Testo : sleep");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("--------------------------------------------------------------- Testo (loc2) : " + loc);
+        //lm = pos.getLocation();
+        GeoPoint startPoint = new GeoPoint(loc);
         mapController.setCenter(startPoint);
-
-        //créer un marqueur
-        Marker startMarker = new Marker(mMapView);
-        //choisir sa ses coordonees
-        startMarker.setPosition(startPoint);
-        //affichage
-        startMarker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM);
-        //affichage en cliquant sur le point
-        startMarker.setTitle("Start point");
-
-        Marker newMarker = new Marker(mMapView);
-        GeoPoint newPoint = new GeoPoint(50.000,-1.000,2944);
-        newMarker.setPosition(newPoint);
-        newMarker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM);
-        mMapView.getOverlays().add(startMarker);
-        mMapView.getOverlays().add(newMarker);
-        mMapView.invalidate();
-
-
 
         return mMapView;
     }
 
 }
-
---
+++ app/src/main/java/com/handipressante/handipressante/MyLocation.java
@@ -0,0 +1,362 @@
+package com.handipressante.handipressante;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.*;
+
+/**
+ * Created by Yvonnig on 21/10/2015.
+ */
+public class MyLocation {
+    Timer timer1;
+    LocationManager lm;
+    LocationResult locationResult;
+    //Location _loc;
+    boolean gps_enabled = false;
+    boolean network_enabled = false;
+    Context c;
+
+   /* public Location getLocation(){
+        return _loc;
+    }*/
+    public MyLocation(){
+        timer1 = new Timer();
+        lm = null;
+        locationResult = new LocationResult();
+    }
+    public boolean searchLocation(Context context, LocationResult result) {
+        // I use LocationResult callback class to pass location value from MyLocation to user code.
+        locationResult = result;
+        if(context == null){
+            System.out.println("------------------------------------------------- Testo : context null");
+            return false;
+        }
+
+        if (lm == null)
+            lm = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
+
+        // Exceptions will be thrown if the provider is not permitted.
+        try {
+            gps_enabled = lm.isProviderEnabled(LocationManager.GPS_PROVIDER);
+        }
+        catch (Exception ex) {
+        }
+        try {
+            network_enabled = lm.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+        }
+        catch (Exception ex) {
+        }
+
+        // Don't start listeners if no provider is enabled.
+        if (!gps_enabled && !network_enabled) {
+            System.out.println("------------------------------------------------- Testo : gps non activé");
+            return false;
+        }
+
+        if (gps_enabled && ContextCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
+                && ContextCompat.checkSelfPermission(context, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED)
+            lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0,
+                    locationListenerGps);
+        if (network_enabled && ContextCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
+                && ContextCompat.checkSelfPermission(context, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED)
+            lm.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0,
+                    locationListenerNetwork);
+        timer1 = new Timer();
+        System.out.println("------------------------------------------------- Testo : Recherche de la loc");
+        c = context;
+        timer1.schedule(new GetLastLocation(), 500);
+        return true;
+    }
+
+    LocationListener locationListenerGps = new LocationListener() {
+        public void onLocationChanged(Location location) {
+            timer1.cancel();
+            locationResult.gotLocation(location);
+            if(ContextCompat.checkSelfPermission(MyApplication.getAppContext(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
+                    && ContextCompat.checkSelfPermission(MyApplication.getAppContext(), Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                    lm.removeUpdates(this);
+                    lm.removeUpdates(locationListenerNetwork);
+                }
+        }
+
+        public void onProviderDisabled(String provider) {
+        }
+
+        public void onProviderEnabled(String provider) {
+        }
+
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+        }
+    };
+
+    LocationListener locationListenerNetwork = new LocationListener() {
+        public void onLocationChanged(Location location) {
+            timer1.cancel();
+            locationResult.gotLocation(location);
+            if(ContextCompat.checkSelfPermission(MyApplication.getAppContext(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
+                    && ContextCompat.checkSelfPermission(MyApplication.getAppContext(), Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                lm.removeUpdates(this);
+                lm.removeUpdates(locationListenerGps);
+            }
+        }
+
+        public void onProviderDisabled(String provider) {
+        }
+
+        public void onProviderEnabled(String provider) {
+        }
+
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+        }
+    };
+
+    class GetLastLocation extends TimerTask {
+        @Override
+        public void run() {
+            System.out.println("------------------------------------------------- Testo : lancement de la recherche");
+            if(ContextCompat.checkSelfPermission(c, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
+                    && ContextCompat.checkSelfPermission(c, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                lm.removeUpdates(locationListenerGps);
+                lm.removeUpdates(locationListenerNetwork);
+            }
+            Location net_loc = null, gps_loc = null;
+            if (gps_enabled)
+                gps_loc = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+            if (network_enabled)
+                net_loc = lm.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+            // If there are both values, use the latest one.
+            if (gps_loc != null && net_loc != null) {
+                if (gps_loc.getTime() > net_loc.getTime())
+                    locationResult.gotLocation(gps_loc);
+                else
+                    locationResult.gotLocation(net_loc);
+                System.out.println("------------------------------------------------- Testo : gps & réseau dispo");
+                return;
+            }
+
+            if (gps_loc != null) {
+                locationResult.gotLocation(gps_loc);
+                System.out.println("------------------------------------------------- Testo : gps dispo");
+                return;
+            }
+            if (net_loc != null) {
+                locationResult.gotLocation(net_loc);
+                System.out.println("------------------------------------------------- Testo : réseau dispo");
+                return;
+            }
+            System.out.println("------------------------------------------------- Testo : pas de loc trouvée");
+            locationResult.gotLocation(null);
+        }
+    }
+
+    public static class LocationResult {
+        MapFragment mF;
+
+        public void setMap(MapFragment m){
+            mF = m;
+        }
+
+        public void gotLocation(Location location){
+            System.out.println("------------------------------------------------- Testo : gotLocation");
+            mF.setLoc(location);
+        }
+       // public Location getLocation{return _loc;}
+    }
+}
+
+
+
+
+
+
+
+
+
+/*
+class MyLocation {
+    Timer timer1;
+    LocationManager lm;
+    //LocationResult locationResult;
+    Location loc;
+    boolean gps_enabled = false;
+    boolean network_enabled = false;
+
+   /* public LocationResult getLocationResult(){
+        return locationResult;
+    }
+
+    public Location getLocation(){
+        return loc;
+    }
+/*
+    public boolean getLocation(Context context, LocationResult result) {
+        // I use LocationResult callback class to pass location value from MyLocation to user code.
+        locationResult = result;
+        if (lm == null)
+            lm = (LocationManager) context
+                    .getSystemService(Context.LOCATION_SERVICE);
+
+        // Exceptions will be thrown if the provider is not permitted.
+        try {
+            gps_enabled = lm.isProviderEnabled(LocationManager.GPS_PROVIDER);
+        } catch (Exception ex) {
+        }
+        try {
+            network_enabled = lm
+                    .isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+        } catch (Exception ex) {
+        }
+
+        // Don't start listeners if no provider is enabled.
+        if (!gps_enabled && !network_enabled)
+            return false;
+
+        if (gps_enabled && ContextCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
+                && ContextCompat.checkSelfPermission(context, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED)
+           lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0,
+                locationListenerGps);
+        if (network_enabled && ContextCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
+                && ContextCompat.checkSelfPermission(context, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED)
+            lm.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0,
+                    locationListenerNetwork);
+        timer1 = new Timer();
+        timer1.schedule(new GetLastLocation(), 5000);
+        return true;
+    }
+
+    public Location getLocation(Context context, Location result) {
+        // I use LocationResult callback class to pass location value from MyLocation to user code.
+        loc = result;
+        if (lm == null)
+            lm = (LocationManager) context
+                    .getSystemService(Context.LOCATION_SERVICE);
+
+        // Exceptions will be thrown if the provider is not permitted.
+        try {
+            gps_enabled = lm.isProviderEnabled(LocationManager.GPS_PROVIDER);
+        } catch (Exception ex) {
+        }
+        try {
+            network_enabled = lm
+                    .isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+        } catch (Exception ex) {
+        }
+
+        // Don't start listeners if no provider is enabled.
+        if (!gps_enabled && !network_enabled)
+            return null;
+
+        if (gps_enabled && ContextCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
+                && ContextCompat.checkSelfPermission(context, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED)
+            lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0,
+                    locationListenerGps);
+        if (network_enabled && ContextCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
+                && ContextCompat.checkSelfPermission(context, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED)
+            lm.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0,
+                    locationListenerNetwork);
+        timer1 = new Timer();
+        timer1.schedule(new GetLastLocation(), 5000);
+        return loc;
+    }
+
+    LocationListener locationListenerGps = new LocationListener() {
+        public void onLocationChanged(Location location) {
+            timer1.cancel();
+            loc=location;
+            if(ContextCompat.checkSelfPermission(MyApplication.getAppContext(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
+                    && ContextCompat.checkSelfPermission(MyApplication.getAppContext(), Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                lm.removeUpdates(this);
+                lm.removeUpdates(locationListenerNetwork);
+            }
+        }
+
+        public void onProviderDisabled(String provider) {
+        }
+
+        public void onProviderEnabled(String provider) {
+        }
+
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+        }
+    };
+
+    LocationListener locationListenerNetwork = new LocationListener() {
+        public void onLocationChanged(Location location) {
+            timer1.cancel();
+            loc=location;
+            if(ContextCompat.checkSelfPermission(MyApplication.getAppContext(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
+                    && ContextCompat.checkSelfPermission(MyApplication.getAppContext(), Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                lm.removeUpdates(this);
+                lm.removeUpdates(locationListenerGps);
+            }
+        }
+
+        public void onProviderDisabled(String provider) {
+        }
+
+        public void onProviderEnabled(String provider) {
+        }
+
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+        }
+    };
+
+    public class GetLastLocation extends TimerTask {
+        @Override
+        public void run() {
+            if(ContextCompat.checkSelfPermission(MyApplication.getAppContext(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
+                    && ContextCompat.checkSelfPermission(MyApplication.getAppContext(), Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                lm.removeUpdates(locationListenerGps);
+                lm.removeUpdates(locationListenerNetwork);
+            }
+
+
+            Location net_loc = null, gps_loc = null;
+            if (gps_enabled)
+                gps_loc = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+            if (network_enabled)
+                net_loc = lm.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+
+            // If there are both values, use the latest one.
+            if (gps_loc != null && net_loc != null) {
+                if (gps_loc.getTime() > net_loc.getTime())
+                    loc=gps_loc;
+                else
+                    loc=net_loc;
+                return;
+            }
+
+            if (gps_loc != null) {
+                loc=gps_loc;
+                return;
+            }
+            if (net_loc != null) {
+                loc=net_loc;
+                return;
+            }
+            loc=null;
+        }
+
+    }
+/*
+    public class LocationResult{
+        Location loc;
+        public Location getLocation(){
+            return loc;
+        }
+        public void gotLocation(Location location){
+            loc = location;
+        }
+    }
+}*/
+
+
