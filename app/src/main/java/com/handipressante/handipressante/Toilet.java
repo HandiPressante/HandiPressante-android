80a920af28e8511f149558b500f4256ae4853e13
Tom GIRAUDET <tom.giraudet@insa-rennes.Fr>
2015-10-23 14:35:16 +0200

    changing decimal format of distance

--- app/src/main/java/com/handipressante/handipressante/Toilet.java
+++ app/src/main/java/com/handipressante/handipressante/Toilet.java
@@ -1,5 +1,7 @@
 package com.handipressante.handipressante;
 
+import java.text.DecimalFormat;
+
 /**
  * Created by Nico on 19/10/2015.
  */
@@ -63,18 +65,41 @@ public class Toilet implements IMarker {
     }
 
     // return distance in meters
-    public  Double getDistance(GPSCoordinates ref) {
+    public  Double getDistance(GPSCoordinates ref, int Gitan) {
         // TODO : compute dist between _coord and ref
-        return new Double(520);
+        switch (Gitan) {
+            case 0:
+                return (double) 167.23;
+            case 1:
+                return (double) 367.56;
+            case 2:
+                return (double) 641;
+            case 3:
+                return (double) 871;
+            case 4:
+                return (double) 1203.23;
+            case 5:
+                return (double) 1607.23;
+            case 6:
+                return (double) 2168.23;
+            case 7:
+                return (double) 2767.23;
+            case 8:
+                return (double) 3132.23;
+            case 9:
+                return (double) 3385.23;
+            default:
+                return (double) 4345.5;
+        }
     }
 
     // return a string to display the distance
     public String getDistanceToString(GPSCoordinates ref) {
-        Double dist = this.getDistance(ref);
+        Double dist = this.getDistance(ref, this._id);
         if(dist>1000){
             // Kilometers
             dist = dist/1000;
-            return "à " + dist +" Km";
+            return "à " + (new DecimalFormat(".##").format(dist)) +" Km";
         }else{
             // meters
             return "à " + dist.intValue() +" m";
