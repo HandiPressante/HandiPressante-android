43687786e9616ddb70a3c665f1c21eb1c5617647
wagam <gautier24@gmail.com>
2015-10-23 09:53:58 +0200

    Fusion des versions punaise et centrage (yvo et marco)

--- app/src/main/java/com/handipressante/handipressante/MapFragment.java
+++ app/src/main/java/com/handipressante/handipressante/MapFragment.java
@@ -1,45 +1,45 @@
 package com.handipressante.handipressante;
 
 import org.osmdroid.tileprovider.tilesource.ITileSource;
-        import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
-        import org.osmdroid.views.overlay.MinimapOverlay;
-        import org.osmdroid.views.overlay.ScaleBarOverlay;
-        import org.osmdroid.views.overlay.compass.CompassOverlay;
-        import org.osmdroid.views.overlay.compass.InternalCompassOrientationProvider;
-        import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider;
-        import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay;
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.views.overlay.MinimapOverlay;
+import org.osmdroid.views.overlay.ScaleBarOverlay;
+import org.osmdroid.views.overlay.compass.CompassOverlay;
+import org.osmdroid.views.overlay.compass.InternalCompassOrientationProvider;
+import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay;
 
-        import android.Manifest;
-        import android.annotation.TargetApi;
-        import android.content.Context;
-        import android.content.SharedPreferences;
-        import android.content.pm.PackageManager;
-        import android.location.Address;
-        import android.location.Geocoder;
-        import android.location.Location;
-        import android.location.LocationListener;
-        import android.location.LocationManager;
-        import android.location.LocationProvider;
-        import android.os.Build;
-        import android.os.Bundle;
-        import android.os.Message;
-        import android.support.v4.app.Fragment;
-        import android.support.v4.app.FragmentManager;
-        import android.support.v4.content.ContextCompat;
-        import android.util.AttributeSet;
-        import android.util.DisplayMetrics;
-        import android.util.Log;
-        import android.view.LayoutInflater;
-        import android.view.Menu;
-        import android.view.MenuInflater;
-        import android.view.MenuItem;
-        import android.view.MenuItem.OnMenuItemClickListener;
-        import android.view.SubMenu;
-        import android.support.v4.app.Fragment;
+import android.Manifest;
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.location.LocationProvider;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Message;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.content.ContextCompat;
+import android.util.AttributeSet;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.MenuItem.OnMenuItemClickListener;
+import android.view.SubMenu;
+import android.support.v4.app.Fragment;
 
 import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.graphics.drawable.Drawable;
 import android.location.Location;
 import android.location.LocationManager;
@@ -50,9 +50,6 @@ import android.graphics.Canvas;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.content.res.ResourcesCompat;
 import android.view.LayoutInflater;
-
-
-
 import android.view.View;
 import android.view.ViewDebug;
 import android.view.ViewGroup;
@@ -60,19 +57,24 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import org.osmdroid.ResourceProxy;
+import org.osmdroid.bonuspack.routing.RoadNode;
 import org.osmdroid.api.IMapController;
 import org.osmdroid.bonuspack.kml.StyleMap;
 import org.osmdroid.bonuspack.overlays.Marker;
+import org.osmdroid.bonuspack.overlays.Polyline;
+import org.osmdroid.bonuspack.routing.OSRMRoadManager;
+import org.osmdroid.bonuspack.routing.RoadManager;
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
 import org.osmdroid.util.GeoPoint;
 import org.osmdroid.util.ResourceProxyImpl;
 import org.osmdroid.views.MapController;
 import org.osmdroid.views.MapView;
-
+import org.osmdroid.bonuspack.routing.Road;
 import org.osmdroid.views.overlay.ItemizedIconOverlay;
 import org.osmdroid.views.overlay.ItemizedIconOverlay.OnItemGestureListener;
 import org.osmdroid.views.overlay.MyLocationOverlay;
 import org.osmdroid.views.overlay.OverlayItem;
+import org.osmdroid.views.overlay.PathOverlay;
 import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider;
 import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay;
 import java.io.IOException;
@@ -96,16 +98,16 @@ public class MapFragment extends Fragment {
     //MyLocation.LocationResult pos = mloc.getLocationResult();
 
     //initialiser startPoint
-    public Location mylocation;
+    //public Location mylocation;
 
     //Coordonnées par defaut
     public LocationManager locationManager;
-    Location location = null;
+    //Location location = null;
     ArrayList<OverlayItem> anotherOverlayItemArray;
-
+    //donne la position courante(provenant de myLocation) à loc
     public void setLoc(Location _loc){
         loc = _loc;
-        System.out.println("--------------------------------------------------------------- Testo (loc) : "+ loc);
+     /*   System.out.println("--------------------------------------------------------------- Testo (loc) : "+ loc);*/
     }
     
     @Override
@@ -116,6 +118,7 @@ public class MapFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         mResourceProxy = new ResourceProxyImpl(inflater.getContext().getApplicationContext());
+
         mMapView = new MapView(inflater.getContext(), 256, mResourceProxy);
 
         
@@ -132,7 +135,7 @@ public class MapFragment extends Fragment {
 
         GpsMyLocationProvider imlp = new GpsMyLocationProvider(this.getContext());
 
-        // public void setLocationUpdateMinDistance(final float meters) (distance de mise a jour
+        // public void setLocationUpdateMinDistance(final float meters) (distance de mise a jour)
         imlp.setLocationUpdateMinDistance(10);
 
         //choisir le point centre du depart
@@ -144,18 +147,19 @@ public class MapFragment extends Fragment {
         mloc.locationResult.setMap(this);
         mloc.searchLocation(getContext(), mloc.locationResult);
         try {
-            Thread.sleep(600);
-            System.out.println("-------------------- Testo : sleep");
+            Thread.sleep(1000);
+        /*    System.out.println("-------------------- Testo : sleep");*/
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        System.out.println("--------------------------------------------------------------- Testo (loc2) : " + loc);
+       /* System.out.println("--------------------------------------------------------------- Testo (loc2) : " + loc);*/
         //lm = pos.getLocation();
         GeoPoint startPoint = new GeoPoint(loc);
         mapController.setCenter(startPoint);
 
         //créer un marqueur
         Marker startMarker = new Marker(mMapView);
+
         //choisir sa ses coordonees
         startMarker.setPosition(startPoint);
         //affichage
@@ -166,12 +170,53 @@ public class MapFragment extends Fragment {
         startMarker.setIcon(getResources().getDrawable(R.drawable.mymarker));
 
         Marker newMarker = new Marker(mMapView);
-        GeoPoint newPoint = new GeoPoint(50.000,-1.000,2944);
+        GeoPoint newPoint = new GeoPoint(48.15,-1.07,2944);
         newMarker.setPosition(newPoint);
         newMarker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM);
+        startMarker.setTitle("Point test");
+
         mMapView.getOverlays().add(startMarker);
         mMapView.getOverlays().add(newMarker);
+
+
+        //navigation
+        //création de manager de navigation
+        final RoadManager roadManager = new OSRMRoadManager();
+        roadManager.addRequestOption("routeType=fastest");
+        //ajout des points de depart et d'arrivee
+        ArrayList<GeoPoint> waypoints = new ArrayList<GeoPoint>();
+        waypoints.add(startPoint);
+        GeoPoint endPoint = new GeoPoint(48.15, -1.63);
+        waypoints.add(endPoint);
+
+        Marker end = new Marker(mMapView);
+        end.setPosition(endPoint);
+        end.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM);
+        mMapView.getOverlays().add(end);
+
+        Road road = roadManager.getRoad(waypoints);
+        Polyline roadOverlay = RoadManager.buildRoadOverlay(road, mMapView.getContext() );
+        roadOverlay.setColor(Color.RED);
+        mMapView.getOverlays().add(roadOverlay);
+
+        Drawable nodeIcon = getResources().getDrawable(R.drawable.mymarker);
+        for (int i=0; i<road.mNodes.size(); i++){
+            RoadNode node = road.mNodes.get(i);
+            Marker nodeMarker = new Marker(mMapView);
+            nodeMarker.setPosition(node.mLocation);
+            nodeMarker.setIcon(nodeIcon);
+            nodeMarker.setTitle("Step "+i);
+            mMapView.getOverlays().add(nodeMarker);
+        }
+        //pour rafraichir la carte
         mMapView.invalidate();
+
+
+
+
+
+
+
 
 
 
