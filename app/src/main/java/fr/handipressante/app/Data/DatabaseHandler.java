6b6d559789fd9d8a18d9851c32538cc40ac6d663
Nico-lb <nicolb.dev@gmail.com>
2016-12-30 19:46:31 +0100

    Updated database version

--- app/src/main/java/fr/handipressante/app/Data/DatabaseHandler.java
+++ app/src/main/java/fr/handipressante/app/Data/DatabaseHandler.java
@@ -9,7 +9,7 @@ import android.database.sqlite.SQLiteOpenHelper;
  */
 public class DatabaseHandler extends SQLiteOpenHelper {
     private static final String DATABASE_NAME = "handipressante";
-    private static final int DATABASE_VERSION = 1;
+    private static final int DATABASE_VERSION = 2;
 
     public DatabaseHandler(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
