a7d00e72bae86238ac302c5f95c8c0e8abf2aa9b
Nico-lb <nicolb.dev@gmail.com>
2016-12-31 18:56:52 +0100

    Removed toilet alert button (for now)

---
+++ app/src/main/java/fr/handipressante/app/edit/ConfirmAlertPhotoDialogFragment.java
@@ -0,0 +1,42 @@
+package fr.handipressante.app.edit;
+
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.support.v7.app.AlertDialog;
+
+import fr.handipressante.app.R;
+
+public class ConfirmAlertPhotoDialogFragment extends DialogFragment {
+    public interface ConfirmPhotoDialogListener {
+        void onDialogPositiveClick(DialogFragment dialog);
+        void onDialogNegativeClick(DialogFragment dialog);
+    }
+
+    private ConfirmPhotoDialogListener mListener;
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setMessage(R.string.confirm_photo_add)
+                .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        mListener.onDialogPositiveClick(ConfirmAlertPhotoDialogFragment.this);
+                    }
+                })
+                .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        mListener.onDialogNegativeClick(ConfirmAlertPhotoDialogFragment.this);
+                    }
+                });
+
+        return builder.create();
+    }
+
+    public void setListener(ConfirmPhotoDialogListener listener) {
+        mListener = listener;
+    }
+}
